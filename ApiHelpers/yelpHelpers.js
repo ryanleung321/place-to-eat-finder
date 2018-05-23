c0d0de17fd756949fae058903cab1b1d4f2ac492
ryanleung321 <ryanleung321@gmail.com>
2018-05-23 04:28:14 -0400

    Updated Yelp API handlers to new API auth (#7)

--- ApiHelpers/yelpHelpers.js
+++ ApiHelpers/yelpHelpers.js
@@ -1,62 +1,21 @@
 'use strict';
 
-const NodeCache = require('node-cache');
 const network = require('./network');
-const YELP_CACHE_KEY = 'yelp';
 
 // YELP API CONSTANTS
-const YELP_CLIENT_ID = process.env.YELP_CLIENT_ID || require('../devConstants').YELP_CLIENT_ID;
-const YELP_CLIENT_SECRET = process.env.YELP_CLIENT_SECRET || require('../devConstants').YELP_CLIENT_SECRET;
+const YELP_API_KEY = process.env.YELP_API_KEY || require('../devConstants').YELP_API_KEY;
 const YELP_API_URL = 'https://api.yelp.com/v3/businesses/search';
-const YELP_AUTH_API = 'https://api.yelp.com/oauth2/token';
-const YELP_AUTH_GRANT_TYPE = 'client_credentials';
-
-// YELP TOKEN CACHE (used for saving Yelp Auth Token)
-const yelpCache = new NodeCache();
-
-// Retrieves the Yelp Access Token either from Auth api (if expired) or from yelpCache
-const getYelpAccessToken = () => {
-  console.log('Function: getYelpAccessToken');
-
-  return new Promise((resolve) => {
-    yelpCache.get(YELP_CACHE_KEY, (err, value) => {
-      if (!err && value) {
-        resolve(value);
-      } else {
-        const requestBody = `grant_type=${YELP_AUTH_GRANT_TYPE}&client_id=${YELP_CLIENT_ID}&client_secret=${YELP_CLIENT_SECRET}`;
-
-        const config = {
-          url: YELP_AUTH_API,
-          headers: {
-            'Accept': 'application/x-www-form-urlencoded',
-            'Content-Type': 'application/x-www-form-urlencoded'
-          },
-          method: 'post',
-          data: requestBody
-        };
-
-        network.call(config).then((resp) => {
-          yelpCache.set(YELP_CACHE_KEY, resp.data.access_token, resp.data.expires_in);
-          resolve(resp.data.access_token);
-        }, (err) => {
-          console.log('error: ', err);
-        });
-      }
-    });
-  });
-};
 
 // Get Yelp search results based on latitude and longitude
 const getYelpSearchResults = (params) => {
   console.log('Function: getYelpSearchResults');
 
-  return getYelpAccessToken().then((yelpAccessToken) => {
     const config = {
       url: YELP_API_URL,
       headers: {
         'Accept': 'application/x-www-form-urlencoded',
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': `Bearer ${yelpAccessToken}`
+        'Authorization': `Bearer ${YELP_API_KEY}`
       },
       method: 'get',
       params,
@@ -67,10 +26,8 @@ const getYelpSearchResults = (params) => {
     }, (err) => {
       console.log('error: ', err);
     });
-  });
 };
 
 module.exports = {
-  getYelpAccessToken,
   getYelpSearchResults,
 };
